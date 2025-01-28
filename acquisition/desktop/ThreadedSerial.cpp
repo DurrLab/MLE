cd5c3d5e0761d605af5c94e8f0b7ee8f2b61d39e
tbobrow1 <tbobrow1@jhmi.edu>
2025-01-27 18:37:36 -0800

    Clen formatting

--- acquisition/desktop/ThreadedSerial.cpp
+++ acquisition/desktop/ThreadedSerial.cpp
@@ -2,74 +2,70 @@
 /*
  *	File name:	ThreadedSerial.cpp
  *
- *	Synopsis:	INSERT
- *
- *	Note:		INSERT
- *
- *	Taylor Bobrow, Johns Hopkins University (2023)
+ *	Taylor Bobrow, Johns Hopkins University (2025)
  *
  */
 
 #include "ThreadedSerial.h"
 
 ThreadedSerial::ThreadedSerial(const std::string port)
-:	mThreadRunning(true)
+: mThreadRunning(true)
 {
-	// connect to serial port
-	char errorOpening = mSerialPort.openDevice(port.c_str(), BAUD_RATE);
+    // connect to serial port
+    char errorOpening = mSerialPort.openDevice(port.c_str(), BAUD_RATE);
 
-	if (!errorOpening)
-		std:: cout << "Device not found at " + port << std::endl;
+    if (!errorOpening)
+        std::cout << "Device not found at " + port << std::endl;
 
-	std::cout << "Device found at " + port << std::endl;
-	
-	// flush the input buffer
-	mSerialPort.flushReceiver();
+    std::cout << "Device found at " + port << std::endl;
 
-	// start the worker thread. */
-	mThread = std::thread(&ThreadedSerial::Start, this);
+    // flush the input buffer
+    mSerialPort.flushReceiver();
+
+    // start the worker thread. */
+    mThread = std::thread(&ThreadedSerial::Start, this);
 }
 
 ThreadedSerial::~ThreadedSerial(void)
 {
-	Stop();
+    Stop();
 }
 
 void ThreadedSerial::AddToTxQueue(output_entry oentry)
 {
-	mOutputQueue.enqueue(oentry);
+    mOutputQueue.enqueue(oentry);
 }
 
 bool ThreadedSerial::GetFromRxQueue(input_entry &ientry)
 {
-	return mInputQueue.try_dequeue(ientry);
+    return mInputQueue.try_dequeue(ientry);
 }
 
 void ThreadedSerial::Start(void)
 {
-	while (mThreadRunning)
-	{
-		output_entry oentry;
-		if (mOutputQueue.try_dequeue(oentry))
-			mSerialPort.writeBytes(&oentry, sizeof(output_entry));
+    while (mThreadRunning)
+    {
+        output_entry oentry;
+        if (mOutputQueue.try_dequeue(oentry))
+            mSerialPort.writeBytes(&oentry, sizeof(output_entry));
 
-		if (mSerialPort.available() > sizeof(input_entry))
-		{
-			input_entry ientry;
-			int error = mSerialPort.readBytes(&ientry, sizeof(input_entry));
-			mInputQueue.enqueue(ientry);
-		}
+        if (mSerialPort.available() > sizeof(input_entry))
+        {
+            input_entry ientry;
+            int error = mSerialPort.readBytes(&ientry, sizeof(input_entry));
+            mInputQueue.enqueue(ientry);
+        }
 
-		// poll every 3 milliseconds
-		std::this_thread::sleep_for(std::chrono::milliseconds(POLLING_INTERVAL));
-	}
+        // poll every 3 milliseconds
+        std::this_thread::sleep_for(std::chrono::milliseconds(POLLING_INTERVAL));
+    }
 }
 
 void ThreadedSerial::Stop(void)
 {
-	mThreadRunning = false;
+    mThreadRunning = false;
 
-	mThread.join();
+    mThread.join();
 
-	mSerialPort.closeDevice();
+    mSerialPort.closeDevice();
 }
